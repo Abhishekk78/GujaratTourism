412bb37fc515b29f8000071d59889ffe645f5315
Abhishek Pandya <147427041+Abhishekk78@users.noreply.github.com>
2024-11-24 13:58:54 +0530

    Gujarat Tourism website complete

---
+++ contact.php
@@ -0,0 +1,35 @@
+
+<?php include '../GujaratTourism-main/contact.html'; ?>
+
+<?php 
+
+ $servername = 'localhost';
+ $username = "root";
+ $password = '';
+ $dbname = 'gujarat_tour';
+
+ if(isset(($_POST['submit'])))
+ {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    $conn = mysqli_connect( $servername,$username,$password,$dbname);
+    $query = "INSERT INTO  contact  VALUES('$name','$email','$message')";
+
+    $data = mysqli_query($conn, $query);
+
+    if($data)
+    {
+        echo " Data send Successfully...";
+    }
+    else
+    {
+        echo " Data is not send Successfully...";
+    }
+
+ }
+?>
+
+
+
